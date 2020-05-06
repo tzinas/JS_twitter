204cad594635b953fa73c84307834f304b274969
tzinas <atzinas2001@gmail.com>
2020-05-06 13:41:14 +0300

    Register

--- app.js
+++ app.js
@@ -6,16 +6,17 @@ const fs = require('fs');
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const FileStore = require('session-file-store')(session);
+const {check, validationResult} = require('express-validator');
 
 var passport = require('passport')
-  , LocalStrategy = require('passport-local').Strategy;
+var LocalStrategy = require('passport-local').Strategy;
 
 app.use("/static", express.static('./static/'));
 app.use(express.static("public"));
 app.use(session({
-    resave: true,
-    secret: "cats",
-    saveUninitialized: true
+  resave: true,
+  secret: "cats",
+  saveUninitialized: true
 }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
@@ -46,6 +47,10 @@ const User = sequelize.define('user', {
     type: DataTypes.STRING,
     allowNull: false
   },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
   password: {
     type: DataTypes.STRING,
     allowNull: false
@@ -53,32 +58,27 @@ const User = sequelize.define('user', {
 }, {
   // options
 });
-
-
-
+/*
 (async () => {
-    await sequelize.sync({ force: true });
-    const tzinas = await User.create({ username: "tzinas", password: "tzinas"});
-    const susan = await User.create({ username: "susan", password: "susan"});
-    //console.log(tzinas.toJSON());
+    await sequelize.sync();
 })();
+*/
 
 passport.use(new LocalStrategy(
-    async function(username, password, done) {
-        try {
-            let user = await User.findOne({where: { username: username }})
-            if (!user) {
-                return done(null, false);
-            }
-            if (user.password != password) {
-                return done(null, false);
-            }
-            return done(null, user);
-        }
-        catch {
-            console.log('Failed to query database');
-        }
-
+  async function(username, password, done) {
+    try {
+      let user = await User.findOne({where: { username }})
+      if (!user) {
+        return done(null, false);
+      }
+      if (user.password != password) {
+        return done(null, false);
+      }
+      return done(null, user);
+    }
+    catch {
+      console.log('Failed to query database');
+    }
   }
 ));
 
@@ -88,10 +88,74 @@ passport.serializeUser(function(user, done) {
 
 passport.deserializeUser(
     async function(id, done) {
-        let user = await User.findOne({where: { id: id }})
+        let user = await User.findOne({where: { id }})
         done(null, user);
     }
 );
+
+
+app.get('/register', function (req, res) {
+  if (req.user){
+    res.redirect('/');
+  }
+  else{
+    res.render('register', {
+      data: {},
+      errors: {}
+    });
+  }
+})
+
+
+app.post('/register', [
+  check('username').isLength({ min: 1 }).withMessage('Insert a username').bail()
+    .custom(async value => {
+      let user = await User.findOne({where: {username:value}})
+      if (user !== null){
+        return Promise.reject();
+      }
+    }).withMessage('Username already used').trim().escape(),
+
+  check('email').isLength({ min: 1 }).withMessage('Insert an email').bail()
+    .isEmail() .withMessage('Incorrect Email').bail().trim().escape().normalizeEmail(),
+
+  check('password').isLength({ min: 1 }).withMessage('Insert a password').bail()
+    .isLength({ min: 3 }).withMessage('Insert a more secure password'),
+
+  check('repeat_password').custom((value, {req}) => (value === req.body.password)).withMessage('Passwords does not match')
+],
+  (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.render('register', {
+      data: req.body,
+      errors: errors.mapped()
+    });
+  }
+  next();
+}, async (req, res, next) => {
+  console.log('Hello');
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log('Hello');
+  await User.sync()
+  const user = await User.create({ username, email, password});
+  req.register_user = user;
+  next();
+}, (req, res) => {
+  console.log('Hello');
+  const user = req.register_user;
+  req.login(user, function(err) {
+    if (err) { return next(err); }
+    return res.redirect('/');
+  });
+
+}
+
+
+);
+
 
 
 app.get('/logout', function(req, res){
@@ -118,12 +182,12 @@ app.post('/login',
 );
 
 app.get('/', (req, res) => {
-    if (req.user){
-        res.render('home', {name: req.user.username});
-    }
-    else{
-        res.redirect('/login');
-    }
+  if (req.user){
+    res.render('home', {name: req.user.username});
+  }
+  else{
+    res.redirect('/login');
+  }
 })
 
 app.listen(3000, () => console.log('Node.js app listening on port 3000.'))
